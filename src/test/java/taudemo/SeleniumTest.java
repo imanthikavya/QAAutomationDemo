6eb95e7ac1e1c552e91250c7aaf5a8761f5f43b8
imanthikavya <imanthikavya1998@gmail.com>
2021-05-23 17:07:25 +0530

    Changes

---
+++ src/test/java/taudemo/SeleniumTest.java
@@ -0,0 +1,48 @@
+package taudemo;
+
+import browser.BrowserGetter;
+import org.junit.jupiter.api.*;
+
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS )
+
+public class SeleniumTest {
+
+        private BrowserGetter browserGetter = new BrowserGetter();
+        private WebDriver driver;
+
+
+        @BeforeAll
+        public void beforeAll(){
+           driver = browserGetter.getChromeDriver();
+
+        }
+        @AfterAll
+
+        public void afterAll(){
+            driver.quit();
+
+        }
+        @Test
+        public void openTheComPageAndCheckTheTitle(){
+
+            String expectedComTitle ="Example Domain";
+            driver.get("https://www.example.com");
+            assertEquals(expectedComTitle,driver.getTitle());
+            //Assertions.assertEquals(1,2);
+        }
+        @Test
+        public void openTheOrgPageAndCheckTheTitle(){
+
+            String expectedOrgTitle ="Example Domain";
+            driver.get("https://www.example.org");
+            assertEquals(expectedOrgTitle,driver.getTitle());
+            //Assertions.assertEquals(1,2 );
+    }
+
+    }
+
---
+++ src/test/java/taudemo/WithAssertion.java
@@ -0,0 +1,21 @@
+package taudemo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+
+@TestInstance(PER_CLASS )
+public class WithAssertion {
+
+    @Test
+    public void firstAssertion(){
+        assertEquals(2,2);
+    }
+    @Test
+    public void secondAssertion(){
+        assertEquals(45,45);
+    }
+}
